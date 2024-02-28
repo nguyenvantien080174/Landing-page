18d95337961e00699c9d204f05982e7af21fd5cc
tien <nguyenvantien080174@gmail.com>
2024-02-28 11:12:54 +0700

    bugg

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//code transition scroll
+const navbar = document.querySelector(".navbar");
+
+function handleScroll(type, offset) {
+  const scrollPosition = window.pageYOffset;
+  alert("Hello world!");
+  
+  if (scrollPosition > offset) {
+    navbar.classList.add(type + "-active");
+  }
+}
+
+function handleClick() {
+  navbar.classList.add("click-active");
+}
+
+window.addEventListener("scroll", handleScroll("nav", 600));
+
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 450) {
+    $(".navbar-fixed-top").addClass("opaque");
+  } else {
+    $(".navbar-fixed-top").removeClass("opaque");
+  }
+});
